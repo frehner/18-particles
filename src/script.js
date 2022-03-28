27220695bdf70925176d657263d2fe2d2d2eeee6
Anthony Frehner <frehner@users.noreply.github.com>
2022-03-27 21:42:40 -0600

    finish lesson

--- src/script.js
+++ src/script.js
@@ -2,35 +2,79 @@ import "./style.css";
 import * as dat from "lil-gui";
 import ReactDOM from "react-dom";
 import * as React from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   PerspectiveCamera,
   OrbitControls,
   useTexture,
-  useHelper,
 } from "@react-three/drei";
+import { AdditiveBlending } from "three";
 
 const { StrictMode, Suspense, useRef } = React;
 
 // Debug
 const gui = new dat.GUI();
 
+function MyParticles() {
+  const [particleTexture] = useTexture(["/textures/particles/2.png"]);
+  const particlesPointRef = useRef();
+  const particlesBufferRef = useRef();
+  useFrame((state) => {
+    const elapsedTime = state.clock.getElapsedTime();
+    // particlesPointRef.current.rotation.y = elapsedTime * 0.2;
+
+    for (let i = 0; i < count; i++) {
+      const i3 = i * 3;
+      const x = particlesBufferRef.current.attributes.position.array[i3 + 0];
+      particlesBufferRef.current.attributes.position.array[i3 + 1] = Math.sin(
+        elapsedTime + x
+      );
+    }
+    particlesBufferRef.current.attributes.position.needsUpdate = true;
+  });
+  return (
+    <points ref={particlesPointRef}>
+      <bufferGeometry args={[1, 32, 32]} ref={particlesBufferRef}>
+        <bufferAttribute
+          attachObject={["attributes", "position"]}
+          array={positions}
+          itemSize={3}
+          count={positions.length / 3}
+        />
+        <bufferAttribute
+          attachObject={["attributes", "color"]}
+          array={colors}
+          itemSize={3}
+          count={colors.length / 3}
+        />
+      </bufferGeometry>
+      <pointsMaterial
+        size={0.1}
+        sizeAttenuation
+        // color="#ff88cc"
+        alphaMap={particleTexture}
+        transparent
+        // alphaTest={0.001}
+        // depthTest={false}
+        depthWrite={false}
+        blending={AdditiveBlending}
+        vertexColors
+      />
+    </points>
+  );
+}
+
 function App() {
   return (
     <StrictMode>
       <Suspense fallback={null}>
         <Canvas gl={{ alpha: false }}>
-          <points>
-            <bufferGeometry args={[1, 32, 32]}>
-              <bufferAttribute
-                attachObject={["attributes", "position"]}
-                array={positions}
-                itemSize={3}
-                count={positions.length / 3}
-              />
-            </bufferGeometry>
-            <pointsMaterial size={0.1} sizeAttenuation />
-          </points>
+          <MyParticles />
+          {/* test cube */}
+          {/* <mesh>
+            <boxBufferGeometry />
+            <meshBasicMaterial />
+          </mesh> */}
           <PerspectiveCamera
             makeDefault
             args={[75, window.innerWidth / window.innerHeight, 0.1, 100]}
@@ -45,8 +89,11 @@ function App() {
 
 const count = 5_000;
 const positions = new Float32Array(count * 3);
+const colors = new Float32Array(count * 3);
+
 for (let i = 0; i < count * 3; i++) {
   positions[i] = (Math.random() - 0.5) * 10;
+  colors[i] = Math.random();
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
